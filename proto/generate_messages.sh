8d78fa747ace785fedb5c87f848634fa8c83084a
Rodrigo Causarano <rjcausarano@gmail.com>
2025-03-11 20:58:23 -0300

    protobuf and protoc from github

--- proto/generate_messages.sh
+++ proto/generate_messages.sh
@@ -2,7 +2,7 @@
 
 THIS_DIR=$(dirname -- "${BASH_SOURCE[0]}")
 pushd $THIS_DIR
-protoc --cpp_out=lite:. comms/*.proto
+../build/_deps/protobuf-build/protoc --cpp_out=lite:. comms/*.proto
 mv comms/*.pb.cc ../src/comms
 mv comms/*.pb.h ../include/comms
 popd
